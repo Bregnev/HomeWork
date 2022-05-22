49473d6f7589991f0e86a2dac9f49def36f41116
Bregnev <105897040+Bregnev@users.noreply.github.com>
2022-05-22 22:30:43 +0700

    First Commit
    First Commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваше имя");
+            string firstName  =  Console.ReadLine();
+
+            Console.WriteLine("Ваша фамилия");
+            string lastName = Console.ReadLine();
+            
+            Console.WriteLine("сколько вам лет");
+            string age = Console.ReadLine();
+
+            string finalText = " вас зовут " + firstName + " ваша фамилия " + lastName + " и прожили вы " + age + " года ";
+            Console.WriteLine(finalText);
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPictures;
+            int photoInRow = 3;
+
+            Console.WriteLine("количество картинок");
+            numberPictures = Convert.ToInt32(Console.ReadLine());
+            
+            int rows = numberPictures / photoInRow;
+            int remainderPhoto = numberPictures % photoInRow;
+            string enter = " количество рядов- " + rows + " сверх меры- " + remainderPhoto + " картинка";
+
+            Console.WriteLine(enter);
+        }
+
+         
+    
+    }
+
+}
